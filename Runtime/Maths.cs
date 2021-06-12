28cf3bbdf343dbeebc41d8f3febe614a8b8e556e
Alice Bottino <s.trippstr@gmail.com>
2021-06-12 02:20:13 -0700

    Added Bezier to Maths

--- Runtime/Maths.cs
+++ Runtime/Maths.cs
@@ -6,6 +6,7 @@ namespace Willow
 {
     public static class Maths
     {
+        #region ====Damp====
         /// <summary>
         /// a frame-independant alternative to Lerp
         /// </summary>
@@ -111,5 +112,20 @@ namespace Willow
             float dt = isRealtime ? Time.unscaledDeltaTime : Time.deltaTime;
             return Quaternion.Slerp(from, to, 1 - Mathf.Exp(-lambda * dt));
         }
+        #endregion
+        #region ====Bezier====
+        public static Vector3 Bezier(Vector3 a, Vector3 b, Vector3 c, float t)
+        {
+            Vector3 p0 = Vector3.Lerp(a, b, t);
+            Vector3 p1 = Vector3.Lerp(b, c, t);
+            return Vector3.Lerp(p0, p1, t);
+        }
+        public static Vector3 Bezier(Vector3 a, Vector3 b, Vector3 c, Vector3 d, float t)
+        {
+            Vector3 p0 = Bezier(a, b, c, t);
+            Vector3 p1 = Bezier(b, c, d, t);
+            return Vector3.Lerp(p0, p1, t);
+        }
+        #endregion
     }
 }
