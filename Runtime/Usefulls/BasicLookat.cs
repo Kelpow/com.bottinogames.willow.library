a2aaaa4eb9b7db3c1957a991c42a4b154627ec0d
Alice Bottino <s.trippstr@gmail.com>
2021-08-17 00:16:33 -0700

    Added BasicLookat

---
+++ Runtime/Usefulls/BasicLookat.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class BasicLookat : MonoBehaviour
+{
+    public Transform target;
+    public Transform upTarget;
+
+#if UNITY_EDITOR
+    public bool runInEditMode = false;
+#endif
+
+    void Update()
+    {
+#if UNITY_EDITOR
+        if (!runInEditMode && !Application.isPlaying)
+            return;
+#endif
+        if (target)
+        {
+            Vector3 up = upTarget ? upTarget.position - transform.position : Vector3.up;
+            
+            transform.LookAt(target, up);
+        }
+    }
+}
