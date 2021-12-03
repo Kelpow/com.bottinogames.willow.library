387b0b855209708c41ba0b2d17596fc3a6c06ccb
Alice Bottino <s.trippstr@gmail.com>
2021-12-03 14:32:04 -0800

    Update Extensions.cs

--- Runtime/Extensions.cs
+++ Runtime/Extensions.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Willow.Library
 {
@@ -139,6 +141,30 @@ namespace Willow.Library
 
             return hit;
         }
+
+
+
+        public static byte[] ToByteArray(this object obj)
+        {
+            if (obj == null)
+                return null;
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream ms = new MemoryStream();
+            bf.Serialize(ms, obj);
+            return ms.ToArray();
+        }
+
+        public static T ToObject<T>(this byte[] arrBytes)
+        {
+            MemoryStream memStream = new MemoryStream();
+            BinaryFormatter binForm = new BinaryFormatter();
+            memStream.Write(arrBytes, 0, arrBytes.Length);
+            memStream.Seek(0, SeekOrigin.Begin);
+            T obj = (T)binForm.Deserialize(memStream);
+            return obj;
+        }
+
+
         #endregion
     }
 }
