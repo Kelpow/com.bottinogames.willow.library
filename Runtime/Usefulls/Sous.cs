b745cfcdf790127975fe83517b6d07b0876f60e2
Alice Bottino <s.trippstr@gmail.com>
2021-12-09 05:55:42 -0800

    Sous multiple layers and alpha blending

--- Runtime/Usefulls/Sous.cs
+++ Runtime/Usefulls/Sous.cs
@@ -14,26 +14,29 @@ public class Sous : MonoBehaviour {
     public Mode mode = Mode.Fill;
     public int width = 320;
     public int height = 240;
-
+    public bool alphaBlend = false;
+    public int depth;
 
     RenderTexture lowResTexture;
-    Texture2D filler;
-    GUIStyle fillerStyle;
 
-	void Start () 
+    void Start () 
     {
         SetResolution(width, height);    
 
         GetComponent<Camera>().allowMSAA = false;
-
-        filler = new Texture2D(1, 1);
-        filler.SetPixel(0, 0, Color.black);
-        filler.Apply();
-
-        fillerStyle = new GUIStyle();
-        fillerStyle.normal.background = filler;
     }
-    
+
+    private void OnEnable()
+    {
+        Willow.SousManager.singleton.AddSousToActive(this);
+    }
+
+    private void OnDisable()
+    {
+        Willow.SousManager.singleton.RemoveSousFromActive(this);
+    }
+
+
     public void SetResolution(int x, int y)
     {
         width = x;
@@ -46,61 +49,6 @@ public class Sous : MonoBehaviour {
         GetComponent<Camera>().targetTexture = lowResTexture;
     }
 
-    void OnGUI() {
-        if (lowResTexture)
-        {
-            GUI.depth = 1001;
-            GUI.Box(new Rect(0,0,Screen.width,Screen.height), GUIContent.none, fillerStyle);
-            GUI.depth = 1000;
-            switch (mode)
-            {
-                case Mode.Fill:
-                    GUI.BeginGroup(new Rect(0, 0, Screen.width, Screen.height));
-                    GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), lowResTexture,ScaleMode.StretchToFill, false);
-                    GUI.EndGroup();
-                    break;
-                case Mode.Fit:
-                    {
-                        float widthScale = (float)Screen.width / (float)lowResTexture.width;
-                        float scaledHeight = widthScale * lowResTexture.height;
-                        if (scaledHeight > Screen.height)
-                        {
-                            float heightScale = (float)Screen.height / (float)lowResTexture.height;
-                            int halfWidth = Screen.width / 2;
-                            int scaledTexWidth = Mathf.FloorToInt(lowResTexture.width * heightScale);
-                            int texHalfWidth = scaledTexWidth / 2;
 
-                            GUI.BeginGroup(new Rect(halfWidth - texHalfWidth, 0, halfWidth - texHalfWidth + scaledTexWidth, Screen.height));
-                            GUI.DrawTexture(new Rect(0, 0, scaledTexWidth, Screen.height), lowResTexture, ScaleMode.StretchToFill, false);
-                            GUI.EndGroup();
-                        } else
-                        {
-                            int halfHeight = Screen.height / 2;
-                            int scaledTexHeight = Mathf.FloorToInt(lowResTexture.height * widthScale);
-                            int texHalfHeight = scaledTexHeight / 2;
-
-                            GUI.BeginGroup(new Rect(0, halfHeight - texHalfHeight, Screen.width, halfHeight - texHalfHeight + scaledTexHeight));
-                            GUI.DrawTexture(new Rect(0, 0, Screen.width, scaledTexHeight), lowResTexture, ScaleMode.StretchToFill, false);
-                            GUI.EndGroup();
-                        }
-
-                    }
-                    break;
-                case Mode.Center:
-                    {
-                        int halfWidth = Screen.width / 2;
-                        int halfHeight = Screen.height / 2;
-                        int texHalfWidth = lowResTexture.width / 2;
-                        int texHalfHeight = lowResTexture.height / 2;
-
-                        GUI.BeginGroup(new Rect(halfWidth - texHalfWidth, halfHeight - texHalfHeight, halfWidth - texHalfWidth + lowResTexture.width, halfHeight - texHalfHeight + lowResTexture.width));
-                        GUI.DrawTexture(new Rect(0, 0, lowResTexture.width, lowResTexture.height), lowResTexture);
-                        GUI.EndGroup();
-                    }
-                    break;
-            }
-        }
-    }
-
-    
+    public RenderTexture GetTex() => lowResTexture;
 }
---
+++ Runtime/Usefulls/SousManager.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Willow
+{
+    public class SousManager : MonoBehaviour
+    {
+        private static SousManager _singleton;
+        public static SousManager singleton
+        {
+            get
+            {
+                if (!_singleton)
+                {
+                    GameObject newManager = new GameObject("Sous Manager", typeof(SousManager));
+                    DontDestroyOnLoad(newManager);
+                    _singleton = newManager.GetComponent<SousManager>();
+                }
+
+                return _singleton;
+            }
+        }
+
+        public List<Sous> activeSous;
+
+        Texture2D filler;
+        GUIStyle fillerStyle;
+
+
+        private void Awake()
+        {
+            activeSous = new List<Sous>();
+        }
+
+        private void Start()
+        {
+            filler = new Texture2D(1, 1);
+            filler.SetPixel(0, 0, Color.black);
+            filler.Apply();
+
+            fillerStyle = new GUIStyle();
+            fillerStyle.normal.background = filler;
+        }
+
+
+        public void AddSousToActive(Sous sous)
+        {
+            activeSous.Add(sous);
+            activeSous.Sort((a, b) => { return a.depth.CompareTo(b.depth); });
+        }
+
+        public void RemoveSousFromActive(Sous sous)
+        {
+            activeSous.Remove(sous);
+        }
+
+
+        void OnGUI()
+        {
+            UnityEngine.GUI.depth = 1001;
+            UnityEngine.GUI.Box(new Rect(0,0,Screen.width,Screen.height), GUIContent.none, fillerStyle);
+            UnityEngine.GUI.depth = 1000;
+            
+            foreach (Sous sous in activeSous)
+            {
+                RenderTexture lowResTexture = sous.GetTex();
+
+                if (lowResTexture)
+                {
+                    switch (sous.mode)
+                    {
+                        case Sous.Mode.Fill:
+                            UnityEngine.GUI.BeginGroup(new Rect(0, 0, Screen.width, Screen.height));
+                            UnityEngine.GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), lowResTexture, ScaleMode.StretchToFill, sous.alphaBlend);
+                            UnityEngine.GUI.EndGroup();
+                            break;
+                        case Sous.Mode.Fit:
+                            {
+                                float widthScale = (float)Screen.width / (float)lowResTexture.width;
+                                float scaledHeight = widthScale * lowResTexture.height;
+                                if (scaledHeight > Screen.height)
+                                {
+                                    float heightScale = (float)Screen.height / (float)lowResTexture.height;
+                                    int halfWidth = Screen.width / 2;
+                                    int scaledTexWidth = Mathf.FloorToInt(lowResTexture.width * heightScale);
+                                    int texHalfWidth = scaledTexWidth / 2;
+
+                                    UnityEngine.GUI.BeginGroup(new Rect(halfWidth - texHalfWidth, 0, halfWidth - texHalfWidth + scaledTexWidth, Screen.height));
+                                    UnityEngine.GUI.DrawTexture(new Rect(0, 0, scaledTexWidth, Screen.height), lowResTexture, ScaleMode.StretchToFill, sous.alphaBlend);
+                                    UnityEngine.GUI.EndGroup();
+                                }
+                                else
+                                {
+                                    int halfHeight = Screen.height / 2;
+                                    int scaledTexHeight = Mathf.FloorToInt(lowResTexture.height * widthScale);
+                                    int texHalfHeight = scaledTexHeight / 2;
+
+                                    UnityEngine.GUI.BeginGroup(new Rect(0, halfHeight - texHalfHeight, Screen.width, halfHeight - texHalfHeight + scaledTexHeight));
+                                    UnityEngine.GUI.DrawTexture(new Rect(0, 0, Screen.width, scaledTexHeight), lowResTexture, ScaleMode.StretchToFill, sous.alphaBlend);
+                                    UnityEngine.GUI.EndGroup();
+                                }
+                            }
+                            break;
+                        case Sous.Mode.Center:
+                            {
+                                int halfWidth = Screen.width / 2;
+                                int halfHeight = Screen.height / 2;
+                                int texHalfWidth = lowResTexture.width / 2;
+                                int texHalfHeight = lowResTexture.height / 2;
+
+                                UnityEngine.GUI.BeginGroup(new Rect(halfWidth - texHalfWidth, halfHeight - texHalfHeight, halfWidth - texHalfWidth + lowResTexture.width, halfHeight - texHalfHeight + lowResTexture.width));
+                                UnityEngine.GUI.DrawTexture(new Rect(0, 0, lowResTexture.width, lowResTexture.height), lowResTexture);
+                                UnityEngine.GUI.EndGroup();
+                            }
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
