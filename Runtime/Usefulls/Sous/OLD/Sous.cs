b106ccae5e72ec6d7f6c061943c77dcf2d595ced
Alice Bottino <s.trippstr@gmail.com>
2023-03-12 12:07:50 -0700

    more sous changes

--- Runtime/Usefulls/Sous/OLD/Sous.cs
+++ Runtime/Usefulls/Sous/OLD/Sous.cs
@@ -2,55 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Willow.Library;
-public class Sous : MonoBehaviour
+
+namespace Willow
 {
-
-    public enum Mode
+    public class Sous : MonoBehaviour
     {
-        Fill,
-        Fit,
-        Center
+        public enum Mode
+        {
+            Fill,
+            Fit,
+            Center
+        }
+
+        public Mode mode = Mode.Fill;
+        public int width = 320;
+        public int height = 240;
+        public bool alphaBlend = false;
+        public int depth;
+
+        RenderTexture lowResTexture;
+
+        void Start()
+        {
+            SetResolution(width, height);
+
+            GetComponent<Camera>().allowMSAA = false;
+        }
+
+        private void OnEnable()
+        {
+            Willow.SousManager.singleton.AddSousToActive(this);
+        }
+
+        private void OnDisable()
+        {
+            Willow.SousManager.singleton.RemoveSousFromActive(this);
+        }
+
+
+        public void SetResolution(int x, int y)
+        {
+            width = x;
+            height = y;
+
+            lowResTexture = new RenderTexture(x, y, 24, RenderTextureFormat.Default);
+            lowResTexture.antiAliasing = 1;
+            lowResTexture.filterMode = FilterMode.Point;
+
+            GetComponent<Camera>().targetTexture = lowResTexture;
+        }
+
+
+        public RenderTexture GetTex() => lowResTexture;
     }
-
-    public Mode mode = Mode.Fill;
-    public int width = 320;
-    public int height = 240;
-    public bool alphaBlend = false;
-    public int depth;
-
-    RenderTexture lowResTexture;
-
-    void Start()
-    {
-        SetResolution(width, height);
-
-        GetComponent<Camera>().allowMSAA = false;
-    }
-
-    private void OnEnable()
-    {
-        Willow.SousManager.singleton.AddSousToActive(this);
-    }
-
-    private void OnDisable()
-    {
-        Willow.SousManager.singleton.RemoveSousFromActive(this);
-    }
-
-
-    public void SetResolution(int x, int y)
-    {
-        width = x;
-        height = y;
-
-        lowResTexture = new RenderTexture(x, y, 24, RenderTextureFormat.Default);
-        lowResTexture.antiAliasing = 1;
-        lowResTexture.filterMode = FilterMode.Point;
-
-        GetComponent<Camera>().targetTexture = lowResTexture;
-    }
-
-
-    public RenderTexture GetTex() => lowResTexture;
 }
-
