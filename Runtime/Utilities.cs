bde8b86a39f3b14ad863855c3dfa110845bf73da
Alice Bottino <s.trippstr@gmail.com>
2023-12-06 02:01:55 -0800

    Adding MakeLineMesh

--- Runtime/Utilities.cs
+++ Runtime/Utilities.cs
@@ -19,5 +19,61 @@ namespace Willow
             }
             action.Invoke(1f);
         }
+
+        public static void MakeLineMesh(Mesh basemesh, Mesh outmesh)
+        {
+            HashSet<(int a, int b)> edges = new HashSet<(int a, int b)>();
+            HashSet<(int a, int b)> doubled = new HashSet<(int a, int b)>();
+            var triangles = basemesh.triangles;
+            for (int i = 0; i < triangles.Length; i += 3)
+            {
+                int a = triangles[i];
+                int b = triangles[i + 1];
+                int c = triangles[i + 2];
+
+                if (edges.Contains((b, a)) || edges.Contains((a, b)))
+                    doubled.Add((a, b));
+                else
+                    edges.Add((a, b));
+
+                if (edges.Contains((c, b)) || edges.Contains((b, c)))
+                    doubled.Add((b, c));
+                else
+                    edges.Add((b, c));
+
+                if (edges.Contains((a, c)) || edges.Contains((c, a)))
+                    doubled.Add((c, a));
+                else
+                    edges.Add((c, a));
+            }
+
+            List<int> newEdges = new List<int>();
+            foreach (var edge in edges)
+            {
+                if (doubled.Contains((edge.a, edge.b)) || doubled.Contains((edge.b, edge.a)))
+                    continue;
+
+                newEdges.Add(edge.a);
+                newEdges.Add(edge.b);
+            }
+            outmesh.SetTriangles(new int[0], 0);
+            outmesh.SetVertices(basemesh.vertices);
+            outmesh.SetNormals(basemesh.normals);
+            outmesh.SetColors(basemesh.colors);
+            outmesh.SetUVs(0, basemesh.uv);
+            outmesh.SetIndices(newEdges, MeshTopology.Lines, 0);
+        }
+
+        public static Mesh MakeNewLineMesh(Mesh basemesh)
+        {
+            if (basemesh == null)
+                return null;
+
+            Mesh newMesh = new Mesh();
+            newMesh.name = basemesh.name + "_EDGE";
+
+            MakeLineMesh(basemesh, newMesh);
+            return newMesh;
+        }
     }
 }
